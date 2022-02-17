e3a61bef35b223297d0a7c22299c3a808c0c154e
Xieym <59227573+X0ym@users.noreply.github.com>
2022-02-17 23:38:52 +0800

    leetcode 688_knightProbability

---
+++ src/leetcode/Solution_688_knightProbability.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+class Solution_688_knightProbability {
+
+    static int[][] dirs = {{-2, -1}, {-2, 1}, {2, -1}, {2, 1}, {-1, -2}, {-1, 2}, {1, -2}, {1, 2}};
+
+    public double knightProbability_1(int n, int k, int row, int column) {
+        // 动态规划
+        double[][][] dp = new double[k + 1][n][n];
+        for (int step = 0; step <= k; step++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (step == 0) {
+                        dp[step][i][j] = 1;
+                    } else {
+                        for (int[] dir : dirs) {
+                            int ni = i + dir[0], nj = j + dir[1];
+                            if (ni >= 0 && ni < n && nj >= 0 && nj < n) {
+                                dp[step][i][j] += dp[step - 1][ni][nj] / 8;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return dp[k][row][column];
+    }
+}
