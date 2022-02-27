6fed593571b6ea670054264fba267a0c9c7f129f
yimingxie <yimingxie@58.com>
2022-02-27 16:45:21 +0800

    Solution_553_optimalDivision

---
+++ src/leetcode/Solution_553_optimalDivision.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+class Solution_553_optimalDivision {
+    public String optimalDivision(int[] nums) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(nums[0]);
+        if (nums.length == 1) return sb.toString();
+        sb.append("/");
+        if (nums.length == 2) return sb.append(nums[1]).toString();
+        sb.append("(");
+        for (int i = 1; i < nums.length; i ++) {
+            sb.append(nums[i] + "/");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        sb.append(")");
+        return sb.toString();
+    }
+}
