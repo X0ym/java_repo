38bcb4991cec409539656ebb0bb6fd0828fb69e0
Xieym <59227573+X0ym@users.noreply.github.com>
2022-03-04 23:19:25 +0800

    Solution_2104_subArrayRanges

---
+++ src/leetcode/daily/Solution_2104_subArrayRanges.java
@@ -0,0 +1,17 @@
+package leetcode.daily;
+
+class Solution_2104_subArrayRanges {
+    public long subArrayRanges(int[] nums) {
+        int n = nums.length;
+        long ret = 0;
+        for (int i = 0; i < n; i++) {
+            int minVal = Integer.MAX_VALUE, maxVal = Integer.MIN_VALUE;
+            for (int j = i; j < n; j++) {
+                minVal = Math.min(minVal, nums[j]);
+                maxVal = Math.max(maxVal, nums[j]);
+                ret += maxVal - minVal;
+            }
+        }
+        return ret;
+    }
+}
---
+++ src/leetcode/todo/SumOfSubarrayRanges_2104.java
@@ -0,0 +1,9 @@
+package leetcode.todo;
+
+/**
+ * https://leetcode-cn.com/problems/sum-of-subarray-ranges/
+ *
+ */
+public class SumOfSubarrayRanges_2104 {
+
+}
